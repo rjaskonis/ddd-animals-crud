95f3aa148e35dfef9db2abb8bbb489df68cb5218
Renne Jaskonis <rjaskonis@gmail.com>
2021-05-14 17:48:22 -0300

    Created create animal http controller

--- src/modules/animals/use-cases/create-animal.controller.ts
+++ src/modules/animals/use-cases/create-animal.controller.ts
@@ -1,14 +1,21 @@
-import { Controller } from "@/shared/infra/controller";
+import { Result } from "@/shared/core/result";
+import { HttpController } from "@/shared/infra/http/controller";
+import { HttpRequest, HttpResponse } from "@/shared/infra/http/transfer";
 import { Request, Response } from "express";
 import { AnimalData } from "../entity/data";
-import { CreateAnimalUseCase } from "./create-animal.interactor";
+import { CreateAnimalUseCase } from "./create-animal.use-case";
+export class CreateAnimalController extends HttpController<CreateAnimalUseCase> {
+    constructor(useCase: CreateAnimalUseCase) {
+        super(useCase);
+    }
 
-export class CreateAnimalController implements Controller {
-    constructor(private readonly useCase: CreateAnimalUseCase) {}
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const animalData: AnimalData = httpRequest.body as AnimalData;
 
-    execute(req: Request, res: Response): Promise<void> {
-        this.useCase.execute({ ...req.body } as AnimalData);
+        const result = await this.useCase.execute(animalData);
 
-        return new Promise((resolve) => resolve());
+        if (result.isFailure) return HttpResponse.badRequest(result.error);
+
+        return HttpResponse.ok(result.getValue());
     }
 }
